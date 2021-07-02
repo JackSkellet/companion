cc2c9e048c002dd044dbbdd01d9c225dd12e1759
jack <kjetilaberge@hotmail.com>
2021-07-02 15:47:04 +0200

    website works again, make jmhub reboot file executable

--- br-webui/index.js
+++ br-webui/index.js
@@ -1698,6 +1698,20 @@ io.on('connection', function(socket) {
 		});
 	});
 
+    socket.on('makeX', function(data) {
+	    logger.log('Making rebootfile Executable');
+		child_process.exec('sudo chmod +x $HOME/companion/scripts/jmhubrestart.sh', function (error, stdout, stderr) {
+			logger.log(stdout + stderr);
+		});
+	});
+
+    socket.on('reboothub', function(data) {
+		logger.log(_companion_directory + '/scripts/jmhubrestart.sh');
+		var cmd = child_process.spawn(_companion_directory + '/scripts/jmhubrestart.sh', {
+			detached: true
+		});
+	});
+
     socket.on('reboothub', function(data) {
 		logger.log(_companion_directory + '/scripts/jmhubrestart.sh');
 		var cmd = child_process.spawn(_companion_directory + '/scripts/jmhubrestart.sh', {
