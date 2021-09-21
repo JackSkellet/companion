c0c4a2f36f3e85ebf38cb204ca96ebc600cb5dcb
jack <kjetilaberge@hotmail.com>
2021-09-21 17:23:31 +0200

    another fuckup

--- scripts/update.sh
+++ scripts/update.sh
@@ -21,6 +21,8 @@ sleep 10
 echo 'adding lock'
 touch $HOME/.updating
 
+echo 'backing up config.ini'
+    cp $HOME/companion/config.ini $HOME/.config.ini
 
 if [ -z "$4" ]; then
     echo 'skipping backup...'
@@ -31,8 +33,6 @@ else
     echo 'backup current repo'
     cp -r $HOME/companion $HOME/.companion
 
-    echo 'backing up config.ini'
-    cp $HOME/companion/config.ini $HOME/.config.ini
 fi
 
 cd $HOME/companion
